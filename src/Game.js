ba203d2b020a69c93afd977496da26b65bd1714e
Dakota Nelson <kotalilyy@gmail.com>
2021-08-21 10:37:16 -0500

    loop over map,  draw wall

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,13 @@
 import TileMap from "./TileMap.js";
 
 const tileSize = 32;
-// entry point for our application
 const canvas = document.getElementById("gameCanvas");
 const ctx = canvas.getContext("2d");
 const tileMap = new TileMap(tileSize);
 
 // define a game loop, it will redraw the screen a certain number of times every one second
 function gameLoop() {
-  tileMap.draw();
+  tileMap.draw(ctx);
 }
 
 tileMap.setCanvasSize(canvas);
--- src/TileMap.js
+++ src/TileMap.js
@@ -20,10 +20,31 @@ export default class TileMap {
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
   ];
 
-  draw(ctx) {}
+  draw(ctx) {
+    for (let row = 0; row < this.map.length; row++) {
+      for (let column = 0; column < this.map[row].length; column++) {
+        let tile = this.map[row][column];
+        if (tile === 1) {
+          // the hash means the method should not be accessible
+          this.#drawWall(ctx, column, row, this.tileSize);
+        }
+      }
+    }
+  }
+
+  #drawWall(ctx, column, row, size) {
+    ctx.drawImage(
+      this.wall,
+      column * this.tileSize,
+      row * this.tileSize,
+      size,
+      size
+    );
+  }
 
   setCanvasSize(canvas) {
     canvas.width = this.map[0].length * this.tileSize;
